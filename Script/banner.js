2c70d54ff9eed5728dc138ce404a16089ae5d764
Hans <hans@gmail.com>
2023-04-03 16:00:16 +0700

    modified:   Script/banner.js 	modified:   Style/homepage.css 	modified:   index.html

--- Script/banner.js
+++ Script/banner.js
@@ -16,3 +16,56 @@ let banners = [
 let banner = document.getElementById("banner")
 let next = document.getElementById("next-btn")
 let prev = document.getElementById("prev-btn")
+let idx = 0
+let auto
+
+function clearBanner() {
+    banner.lastElementChild.remove()
+    banner.lastElementChild.remove()
+}
+
+function setBanner() {
+    let img = document.createElement("img")
+    img.src = banners[idx].image
+    let caption = document.createElement("h3")
+    caption.innerHTML = banners[idx].caption
+
+    banner.appendChild(img)
+    banner.appendChild(caption)
+}
+
+next.addEventListener("click", (e)=>{
+    idx++;
+    if (idx >= banners.length) {
+        idx = 0
+    }
+    clearBanner()
+    setBanner()
+    window.clearInterval(auto)
+    autoSlider()
+})
+
+prev.addEventListener("click", (e)=>{
+    idx--;
+    if (idx < 0) {
+        idx = banners.length-1
+    }
+    clearBanner()
+    setBanner()
+    window.clearInterval(auto)
+    autoSlider()
+})
+
+function autoSlider() {
+    auto = setInterval(()=>{
+        idx++;
+        if (idx >= banners.length) {
+            idx = 0
+        }
+        clearBanner()
+        setBanner()
+    }, 5000)
+}
+
+setBanner()
+autoSlider()
