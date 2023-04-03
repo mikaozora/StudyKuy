2f538612c89d400c9eb9d93f4f347be96d917132
Hans <hans@gmail.com>
2023-04-03 14:43:41 +0700

    new file:   Script/banner.js 	modified:   index.html

---
+++ Script/banner.js
@@ -0,0 +1,18 @@
+let banners = [
+    {
+        image: "assets/images/banner/banner-1.png",
+        caption: "Be a professional programmer with us"
+    },
+    {
+        image: "assets/images/banner/banner-2.png",
+        caption: "Only in 30 days you can built your own app"
+    },
+    {
+        image: "assets/images/banner/banner-3.png",
+        caption: "Learn to secure your data from hacker"
+    }
+]
+
+let banner = document.getElementById("banner")
+let next = document.getElementById("next-btn")
+let prev = document.getElementById("prev-btn")
